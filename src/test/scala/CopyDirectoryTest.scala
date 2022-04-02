de7a15964d6bd755fa9a3ce7d4135bd012bc2c2c
AmirHosseinAbarashi <ah.abarashi@gmail.com>
2022-04-02 19:17:28 +0430

    Edit Test v1

--- src/test/scala/CopyDirectoryTest.scala
+++ src/test/scala/CopyDirectoryTest.scala
@@ -1,8 +1,11 @@
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.prop.TableDrivenPropertyChecks._
 
-import java.io.{File, IOException}
+import java.io.File
 import java.nio.file.{Files, Paths}
 import java.util.Objects
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 object CopyDirectoryTest
 
@@ -11,29 +14,22 @@ class CopyDirectoryTest extends AnyFunSuite {
   val copyDirectory = new CopyDirectory
 
   val pathIn: String = "test-file/test-in"
-  val pathOut1: String = Files.createTempDirectory("copyTest").toString
-  val pathOut2: String = Files.createTempDirectory("copyTest").toString
-  val pathOut3: String = Files.createTempDirectory("copyTest").toString
+  val pathOut: String = Files.createTempDirectory("copyTest").toString
 
-  test("Test1") {
-    copyDirectory.copy(pathIn + "/folder1", pathOut1)
-    assertCopied(pathIn + "/folder1", pathOut1)
-    deleteFiles(new File(pathOut1))
-    Files.delete(Paths.get(pathOut1))
-  }
-
-  test("Test2") {
-    copyDirectory.copy(pathIn + "/folder2", pathOut2)
-    assertCopied(pathIn + "/folder2", pathOut2)
-    deleteFiles(new File(pathOut2))
-    Files.delete(Paths.get(pathOut2))
-  }
-
-  test("Test3") {
-    copyDirectory.copy(pathIn, pathOut3)
-    assertCopied(pathIn, pathOut3)
-    deleteFiles(new File(pathOut3))
-    Files.delete(Paths.get(pathOut3))
+  test("Test") {
+    forAll(
+      Table(
+        "srcFolder",
+        "/folder1",
+        "/folder2",
+        "/"
+      )
+    ) { f =>
+      Await.ready(copyDirectory.copy(pathIn + f, pathOut), Duration("1 day"))
+      assertCopied(pathIn + f, pathOut)
+      deleteFiles(new File(pathOut))
+    }
+    Files.delete(Paths.get(pathOut))
   }
 
   private def assertCopied(sourceDirectoryPath: String, destinationDirectoryPath: String): Unit = { // test
@@ -45,12 +41,10 @@ class CopyDirectoryTest extends AnyFunSuite {
     }
   }
 
-  @throws[IOException]
-  private def deleteFiles(file: File): Unit = { // Delete all files in a folder [test]
-    for (f <- Objects.requireNonNull(file.list)) {
+  private def deleteFiles(file: File): Unit = // Delete all files in a folder [test]
+    for (f <- file.list()) {
       Files.delete(new File(file, f).toPath)
     }
-  }
 
 }
 
